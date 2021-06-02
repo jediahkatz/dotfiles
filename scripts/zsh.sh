fc7cd5f25d8b0f6703c22517a45aa4e9da1bf0d4
Jediah Katz <jediahkatz@gmail.com>
2021-06-02 11:29:10 -0700

    additions during mac setup

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/zsh
+# NOTE: THIS SCRIPT IS NOT CURRENTLY RUNNABLE
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Add symlinks
+ln -s $(pwd)/.alacritty.yml ~/.alacritty.yml
+ln -s $(pwd)/.p10k.zsh ~/.p10k.zsh
+rm ~/.zshrc
+ln -s $(pwd)/.zshrc ~/.zshrc
+
+# TODO: os-specific symlinks
+
+# Ubuntu only
 sudo apt update
 
 sudo add-apt-repository ppa:deadsnakes/ppa
@@ -7,10 +21,21 @@ packages=(
     python3 python3-pip python3.6 libpython3.6-dev python3.7 libpython3.7-dev python3.9 libpython3.9-dev
     yarn
     build-essential zlib1g-dev llvm-dev libclang-dev clang
-    bat lsd ripgrep fzf
+    bat lsd diff-so-fancy ripgrep fzf
+    hub
 )
 
 sudo apt install "$packages"
+
+# OSX only
+packages=(
+    tmux fd
+    yarn
+    bat lsd diff-so-fancy ripgrep fzf
+    hub
+)
+
+brew install $packages
 
 # TODO: add all non-apt stuff:
 # - cargo, nvm, 
@@ -27,3 +52,9 @@ rm -rf tldr-c-client
 
 git clone https://github.com/Aloxaf/fzf-tab ~ZSH_CUSTOM/plugins/fzf-tab
 
+# Install zsh plugins
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+brew install zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
